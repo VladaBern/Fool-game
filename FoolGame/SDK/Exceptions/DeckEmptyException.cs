95a7b7c8d02c9a3cc48831c4a110c271ffc4eb08
Vlada Bernadskaya <vladabernadskaya@gmail.com>
2022-08-16 15:02:56 +0300

    Add class DeckEmptyException

---
+++ FoolGame/SDK/Exceptions/DeckEmptyException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SDK.Exceptions
+{
+    internal class DeckEmptyException : Exception
+    {
+        public DeckEmptyException(string message) : base(message) { }
+    }
+}
