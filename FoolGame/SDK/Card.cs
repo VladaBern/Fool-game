4bd84a449e3912659687ed98cf05b769321f3344
Vlada Bernadskaya <vladabernadskaya@gmail.com>
2022-08-16 15:02:11 +0300

    Add class Card

---
+++ FoolGame/SDK/Card.cs
@@ -0,0 +1,32 @@
+﻿namespace SDK
+{
+    public class Card
+    {
+        public CardSuit Suit { get; }
+        public CardValue Value { get; }
+
+        public Card(CardSuit suit, CardValue value)
+        {
+            this.Suit = suit;
+            this.Value = value;
+        }
+
+        public bool CanFight(Card card, CardSuit trump)
+        {
+            if (this.Suit == card.Suit)
+            {
+                return (int)this.Value > (int)card.Value;
+            }
+            else if (card.Suit == trump)
+            {
+                return false;
+            }
+            else if (this.Suit == trump)
+            {
+                return true;
+            }
+            
+            return false;
+        }
+    }
+}
