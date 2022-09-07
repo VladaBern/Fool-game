7bea7471b13df55efb21d5c14c2bd14ed9dd02bd
Vlada Bernadskaya <vladabernadskaya@gmail.com>
2022-09-07 16:28:51 +0300

    Add class Player

---
+++ FoolGame/SDK/Player.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace SDK
+{
+    public class Player
+    {
+        private string name;
+        private List<Card> cards = new List<Card>();
+
+        public Player(string name)
+        {
+            this.name = name;
+        }
+    }
+}
