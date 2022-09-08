342f628a5931eff110b16736e79cd36233e4927c
Vlada Bernadskaya <vladabernadskaya@gmail.com>
2022-09-08 12:15:20 +0300

    Add InvalidCommandException

---
+++ FoolGame/SDK/Exceptions/InvalidCommandException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SDK.Exceptions
+{
+    public class InvalidCommandException : Exception
+    {
+        public InvalidCommandException(string message) : base(message) { }
+    }
+}
