bcf0607ce182fc69c4c66e0f72f136157bc06aec
Vlada Bernadskaya <vladabernadskaya@gmail.com>
2022-09-08 12:21:33 +0300

    Add implementation to HumanPlayer

--- FoolGame/SDK/Players/HumanPlayer.cs
+++ FoolGame/SDK/Players/HumanPlayer.cs
@@ -1,7 +1,15 @@
-﻿namespace SDK.Players
+﻿using SDK.Commands;
+using SDK.Results;
+
+namespace SDK.Players
 {
     public class HumanPlayer : Player
     {
         public HumanPlayer(string name) : base(name) { }
+
+        public override Result Handle(Command command)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
